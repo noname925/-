24c92ebd5f2e67748041a000adea897323352422
noname <rus.dzyba1996@mail.ru>
2021-10-20 14:28:26 +0300

    Оптимизировал программу 'генерация паролей'

--- generation_password.py
+++ generation_password.py
@@ -1,37 +1,98 @@
 import random
 
 
-def generation_passwort(lengh, chars):
-    return random.sample(chars, lengh)
-
-
 digits = '0123456789'
 lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
 uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 punctuation = '!#$%&*+-=?@^_.'
 chars = ''
 
-counter_password = int(input('Введите количество паролей для генерации: '))
-lengh = int(input('Введите длину пароля: '))
 
-num = input('Включать ли цифры 1234567890? Введите да или нет: ')
-alpha = input('Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? Введите да или нет: ')
-alpha_lower = input('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz? Введите да или нет:')
-sim = input('Включать ли символы !#$%&*+-=?@^_? Введите да или нет: ')
-iskl = input('Исключать ли неоднозначные символы il1Lo0O? Введите да или нет: ')
+def counter_password():
+    counter_password = input('Введите количество паролей для генерации: ')
+    while counter_password.isdigit() == False:
+        print('Нужно ввести цифру')
+        counter_password = input('Введите количество паролей для генерации: ')
+    return int(counter_password)
 
-if num.lower() == 'да':
+
+def lengh():
+    lengh = input('Введите длину пароля: ')
+    while lengh.isdigit() == False:
+        print('Нужно ввести цифру')
+        lengh = input('Введите длину пароля: ')
+    return int(lengh)
+
+
+def num():
+    num = input('Включать ли цифры 1234567890? Введите да или нет: ')
+    while num.lower() not in ('да','нет'):
+        print('Нужно ввести да или нет')
+        num = input('Включать ли цифры 1234567890? Введите да или нет: ')
+    return num
+
+
+def alpha():
+    alpha = input('Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? Введите да или нет: ')
+    while alpha.lower() not in ['да', 'нет']:
+        print('Введите да или нет')
+        alpha = input('Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? Введите да или нет: ')
+    return alpha
+
+
+def alpha_lower():
+    alpha_lower = input('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz? Введите да или нет:')
+    while alpha_lower.lower() not in ('да', 'нет'):
+        print('Введите да или нет')
+        alpha_lower = input('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz? Введите да или нет:')
+    return alpha_lower
+
+
+def sim():
+    sim = input('Включать ли символы !#$%&*+-=?@^_? Введите да или нет: ')
+    while sim.lower() not in ('да', 'нет'):
+        print('Введите да или нет')
+        sim = input('Включать ли символы !#$%&*+-=?@^_? Введите да или нет: ')
+    return sim
+
+
+def iskl():
+    iskl = input('Исключать ли неоднозначные символы il1Lo0O? Введите да или нет: ')
+    while iskl.lower() not in ('да', 'нет'):
+        print('Введите да или нет')
+        iskl = input('Исключать ли неоднозначные символы il1Lo0O? Введите да или нет: ')
+    return iskl
+
+
+def generation_passwort(lengh, chars):
+    return random.sample(chars, lengh)
+
+
+#СТАРТ ПРОГРАММЫ
+
+
+c = counter_password()
+l = lengh()
+n = num()
+a = alpha()
+a1 = alpha_lower()
+s = sim()
+i = iskl()
+
+
+if n == 'да':
     chars += '1234567890'
-if alpha.lower() == 'да':
+if a == 'да':
     chars += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-if alpha_lower.lower() == 'да':
+if a1 == 'да':
     chars += 'abcdefghijklmnopqrstuvwxyz'
-if sim == 'да':
+if s == 'да':
     chars += '!#$%&*+-=?@^_?'
-if iskl == 'да':
-    for c in 'il1Lo0O':
-        chars = chars.replace(c, '')
+if i == 'да':
+    for j in 'il1Lo0O':
+        chars = chars.replace(j, '')
 
-for i in range(1, counter_password + 1):
+
+for i in range(1, c+1):
     print(i, '  ', end=' ')
-    print(*generation_passwort(lengh, chars), sep='')
+    print(*generation_passwort(l, chars), sep='')
